04c667489cb611d3e9f36eb544f736d543677ddf
SankerProtus <sankerprotus04@gmail.com>
2025-04-30 13:21:59 +0000

    Initial commit

--- Dicee-Project/index.js
+++ Dicee-Project/index.js
@@ -33,6 +33,8 @@ function checkWinner() {
             player1Button.innerHTML = 'Roll Dice';
             player2Button.innerHTML = 'Roll Dice';
             heading.innerHTML = 'Dicee Game';
+            document.querySelectorAll('img')[0].setAttribute('src', './images/dice6.png');
+            document.querySelectorAll('img')[1].setAttribute('src', './images/dice6.png');
         }, 2000);
     }
 }
